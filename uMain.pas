8f43c7f7367964f96cca15b525008390cadd1aab
gabr1el1 <gabrielhgxd@gmail.com>
2023-03-30 23:07:05 -0600

    Ahora hay vista para Inverted landscape

--- uMain.pas
+++ uMain.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Objects, FMX.Controls.Presentation;
+  FMX.Objects, FMX.Controls.Presentation,System.Messaging,FMX.Platform;
 
 type
   TfrmMain = class(TForm)
@@ -17,45 +17,69 @@ type
     Panel2: TPanel;
     Image1: TImage;
     Image2: TImage;
-    procedure FormResize(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure DoOrientationChanged(const Sender: TObject; const M: TMessage);
+    procedure FormDestroy(Sender: TObject);
+
   private
     { Private declarations }
+    FId: Integer;
   public
     { Public declarations }
+
   end;
 
 var
   frmMain: TfrmMain;
 
+
 implementation
 
 {$R *.fmx}
 
-procedure TfrmMain.FormResize(Sender: TObject);
-var w, h : single;
+procedure TfrmMain.DoOrientationChanged(const Sender: TObject; const M: TMessage);
+var w,h: single;
+var s: IFMXScreenService;
 begin
- // verificar si hubo un cambio de orientación
- if self.Width > self.Height then
- begin
- // hubo un cambio de orientación a LandScape
- // orientar el panel 1 a la izquierda
- // el panel 2 se ajustará a lo que quede de espacio (Client)
- // dividir el ancho del panel de imagenes en 2 para
- // ajustar el tamaño de ambas imagenes a través de los paneles
- w := pnlImages.Width / 2;
- Panel1.Align := TAlignLayout.Left;
- Panel1.Width := w;
- end
- else if self.Height > self.Width then
- begin
- // hubo un cambio de orientación a Portrait
- // orientar el panel 1 al Tope
- // el panel 2 se ajustará a lo que quede de espacio (Client)
- h := pnlImages.Height / 2;
- Panel1.Align := TAlignLayout.Top;
- Panel1.Height := h;
- end;
-end; // FormResize
+  if TPlatformServices.Current.SupportsPlatformService(IFMXScreenService, s) then begin
+    h:=pnlImages.Height / 2;
+    w:=pnlImages.Width / 2;
+
+    case s.GetScreenOrientation of
+      TScreenOrientation.Portrait:
+      begin
+        Panel1.Height:=h;
+        Panel1.Align:=TAlignLayout.Top;
+      end;
+      TScreenOrientation.Landscape:
+      begin
+        Panel1.Width:=w;
+        Panel1.Align:=TAlignLayout.Left;
+      end;
+      TScreenOrientation.InvertedLandscape:
+      begin
+        Panel1.Width:=w;
+        Panel1.Align:=TAlignLayout.Right;
+      end;
+    end;
+  end;
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  // Subscribe to TOrientationChangedMessage
+FId := TMessageManager.DefaultManager.SubscribeToMessage(TOrientationChangedMessage, DoOrientationChanged);
+end;
+
+procedure TfrmMain.FormDestroy(Sender: TObject);
+begin
+  // Unsubscribe the TOrientationChangedMessage
+TMessageManager.DefaultManager.Unsubscribe(TOrientationChangedMessage, FId);
+end;
+
+end.procedure TfrmMain.Image1Click(Sender: TObject);
+begin
+
+end;
 
 
-end.
